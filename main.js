4fad172f263dbe55ac4d14142b8a4d76dd837d04
sasikumar <45989334+sasikumarsp@users.noreply.github.com>
2020-05-15 10:12:37 +0530

    Update main.js

--- main.js
+++ main.js
@@ -17,8 +17,9 @@ $(document).ready(function() {
         });
   });
 
-
-  SC.stream('/tracks/45800403',function(sound){
+ //Aanantha yaalai
+//<iframe width="100%" height="300" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/107812013&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"></iframe>
+  SC.stream('/tracks/107812013',function(sound){
     $('#start-r2').click(function(e) {
            e.preventDefault();
            sound.start();
